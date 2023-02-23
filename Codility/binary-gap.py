0e97f993c5bdfaa708e896aab808795eb8a1a879
hdong69 <47088945+hdong69@users.noreply.github.com>
2023-02-23 15:11:41 +0700

    Update binary-gap.py

--- Codility/binary-gap.py
+++ Codility/binary-gap.py
@@ -13,3 +13,10 @@ def solution(N):
       if max < sub - 1 :
         max = sub - 1
     return(max)
+
+#Another solution
+#source: https://stackoverflow.com/a/69917131
+def solution(N):
+    b=bin(N)[2:]
+    return len(max(b.strip('0').split('1'))) 
+# here strip will remove all trailing 0's and split will create a list with splitting 1's and combining 0's which are together.
