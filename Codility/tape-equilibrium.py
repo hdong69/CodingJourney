6b2aa6640e7eb44de122f0e68709bd04cb1c75be
hdong69 <47088945+hdong69@users.noreply.github.com>
2023-03-04 22:03:15 +0700

    Add another solution

--- Codility/tape-equilibrium.py
+++ Codility/tape-equilibrium.py
@@ -14,3 +14,20 @@ def solution(A):
 
     x = min(diff_array)
     return(x)
+
+#Another solution
+#source https://stackoverflow.com/a/26822990
+
+def TapeEquilibrium(A):
+    left = A[0]
+    right = sum(A[1::])
+    diff = abs(left - right)
+
+    for p in range(1, len(A)):
+        ldiff = abs(left - right)
+        if ldiff < diff:
+            diff = ldiff
+        left += A[p]
+        right -= A[p]
+
+    return diff
