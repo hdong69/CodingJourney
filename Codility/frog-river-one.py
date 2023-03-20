d47a901e968f8319af2e99f2fa14bb22e98c62de
hdong69 <47088945+hdong69@users.noreply.github.com>
2023-03-20 15:36:29 +0700

    20-03-2023: FrogRiverOne (#7)

---
+++ Codility/frog-river-one.py
@@ -0,0 +1,18 @@
+#source: https://stackoverflow.com/a/70770450
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(X, A): 
+     
+    positions = set() 
+    seconds = 0 
+ 
+    for i in range(0, len(A)): 
+        if A[i] not in positions and A[i] <= X: 
+            positions.add(A[i]) 
+            seconds = i 
+     
+    if len(positions) == X: 
+        return seconds
+
+    return -1
